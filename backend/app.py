fc1cf557d82adcfd611c622708a5d6052e52d86f
tapiopuputti <tapio.puputti@gmail.com>
2020-12-08 14:04:22 +0200

    Added "/<key>/<value>" route to find specific results.

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 # Create a flask object
 app = Flask(__name__)
 
+# Return all results
 @app.route('/api/results')
 def results():
     results = [
@@ -44,19 +45,71 @@ def results():
         }
     ]
 
-    # sort the dictionary according to result time
-    results.sort(key = lambda x: x['result'])
+    sort_list(results)
 
     # Flask requires the return value to be a dictionary, list does not work
     return { "data": [results] }
 
-'''
-@app.route('/tulokset/{ID}:{nimi}')
-def tulokset():
-    # hae sanakirjasta/tietokannasta kilpailijan matkat ja tulokset (myös kuva ym tietoa?) ID:n ja/tai nimen mukaan
-    # haku matkan mukaan?
-    return tulokset
-'''
+# Return specific results: e.g. distance/10km
+@app.route('/<key>/<value>')
+def matka(key, value):
+    
+    results = [
+        {
+            'id' : 0, 
+            'name' : 'Kalle Järvinen',
+            'result' : '01:23:34',
+            'distance' : '10km' 
+        },
+        {
+            'id' : 1, 
+            'name' : 'Pentti Mielonen',
+            'result' : '02:11:04',
+            'distance' : '5km' 
+        },
+        {
+            'id' : 2, 
+            'name' : 'Jukka Keimola',
+            'result' : '01:09:31',
+            'distance' : '10km' 
+        },
+        {
+            'id' : 3, 
+            'name' : 'Heikki Kinnunen',
+            'result' : '02:09:51',
+            'distance' : '10km' 
+        },
+        {
+            'id' : 4, 
+            'name' : 'Olli Ojala',
+            'result' : '01:53:21',
+            'distance' : '1km' 
+        },
+        {
+            'id' : 5, 
+            'name' : 'Veikko Huopalainen',
+            'result' : '03:00:55',
+            'distance' : '1km' 
+        }
+    ]
+
+    # Make a new list and wanted key & value
+    new_list = []
+    #key = 'distance'
+    #value = '10km'
+
+    # Search the dictionary for wanted information and append it to the new list
+    for items in results:
+        if items[key] == value:
+            new_list.append(items)
+
+    sort_list(new_list)
+
+    return { "data": [new_list] }
+
+# Sort list according to result time
+def sort_list(results):
+    return results.sort(key = lambda x: x['result'])
 
 if __name__ == '__main__':
     app.run(debug = True)
