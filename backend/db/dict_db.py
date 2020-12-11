583ae6501d2e52cb6dd55c7e14ba4d781f8e8a15
Arto Mulkahainen <arto.mulkahainen@gmail.com>
2020-12-11 10:01:34 +0200

    dict_db added

---
+++ backend/db/dict_db.py
@@ -0,0 +1,47 @@
+class dict_db:
+    def __init__(self):
+        self.results = [
+            {
+                'id' : 0, 
+                'name' : 'Kalle Järvinen',
+                'result' : '01:23:34',
+                'distance' : '10km' 
+            },
+            {
+                'id' : 1, 
+                'name' : 'Pentti Mielonen',
+                'result' : '02:11:04',
+                'distance' : '10km' 
+            },
+            {
+                'id' : 2, 
+                'name' : 'Jukka Keimola',
+                'result' : '01:09:31',
+                'distance' : '10km' 
+            },
+            {
+                'id' : 3, 
+                'name' : 'Heikki Kinnunen',
+                'result' : '02:09:51',
+                'distance' : '10km' 
+            },
+            {
+                'id' : 4, 
+                'name' : 'Olli Ojala',
+                'result' : '01:53:21',
+                'distance' : '10km' 
+            },
+            {
+                'id' : 5, 
+                'name' : 'Veikko Huopalainen',
+                'result' : '03:00:55',
+                'distance' : '10km' 
+            }
+        ]
+
+    def get_dictionary_data(self):
+        return sort_list(self.results)
+
+    
+    def sort_list(self, results):
+        return results.sort(key = lambda x: x['result'])
